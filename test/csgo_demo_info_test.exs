1f2b243302f02d207b9b32405770ff5154c09a24
tjhiry29 <r.tjhie0809@gmail.com>
2018-03-30 21:41:54 -0400

    update tests

--- test/csgo_demo_info_test.exs
+++ test/csgo_demo_info_test.exs
@@ -1,8 +1,28 @@
 defmodule DemoinfogoTest do
   use ExUnit.Case
-  doctest Demoinfogo
 
-  test "greets the world" do
-    assert Demoinfogo.hello() == :world
+  test "correctly finds trade kills" do
+    first_kill = %DemoInfoGo.Kill{
+      tick: 0,
+      attacker_name: "test1",
+      victim_name: "test2",
+      trade: false
+    }
+
+    second_kill = %DemoInfoGo.Kill{
+      tick: 5 * 128 - 1,
+      attacker_name: "test3",
+      victim_name: "test1",
+      trade: false
+    }
+
+    kill = DemoInfoGo.Kill.find_trades(second_kill, 128, [first_kill])
+    assert kill.trade == true
+  end
+
+  test "correctly assigns first kill" do
+    kills = [%DemoInfoGo.Kill{}, %DemoInfoGo.Kill{}]
+    kills = DemoInfoGo.Kill.find_first_kills(kills)
+    assert Enum.at(kills, 0).first_of_round == true
   end
 end
